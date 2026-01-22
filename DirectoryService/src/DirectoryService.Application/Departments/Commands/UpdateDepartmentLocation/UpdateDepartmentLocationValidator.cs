da573ad77fb327ead99bf9180470ad2b274d4e5e
ivnvch <tarasovets.dmtiv@gmail.com>
2026-01-22 19:09:30 +0300

    feat: update departmentLocations. Added controller end-point and validator

---
+++ DirectoryService/src/DirectoryService.Application/Departments/Commands/UpdateDepartmentLocation/UpdateDepartmentLocationValidator.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.Design.Serialization;
+using DirectoryService.Application.Extensions.Validation;
+using DirectoryService.Application.Locations.Repositories;
+using DirectoryService.Shared.Errors;
+using FluentValidation;
+
+namespace DirectoryService.Application.Departments.Commands.UpdateDepartmentLocation;
+
+public class UpdateDepartmentLocationValidator : AbstractValidator<UpdateDepartmentLocationCommand>
+{
+    private readonly ILocationRepository _locationRepository;
+    
+    public UpdateDepartmentLocationValidator(ILocationRepository locationRepository)
+    {
+        _locationRepository = locationRepository;
+        
+        RuleFor(x => x.LocationIds)
+            .NotNull()
+            .NotEmpty()
+            .MustAsync(async (ids, cancellation) =>
+            {
+                var result = await _locationRepository.AllExistAsync(ids.ToArray(), cancellation);
+                return result is { IsSuccess: true, Value: true };
+            })
+            .WithError(Error.Validation(
+                new ErrorMessage("locationIds.not.exists", "One or more locations are not exists")));
+    }
+}
