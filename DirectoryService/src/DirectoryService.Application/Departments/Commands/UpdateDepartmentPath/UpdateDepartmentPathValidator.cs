77ebf56e7b6c513ba3642cce00604710d4f8654b
ivnvch <tarasovets.dmtiv@gmail.com>
2026-01-27 08:36:29 +0300

    update DepartmentPathValidator

--- DirectoryService/src/DirectoryService.Application/Departments/Commands/UpdateDepartmentPath/UpdateDepartmentPathValidator.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Commands/UpdateDepartmentPath/UpdateDepartmentPathValidator.cs
@@ -1,11 +1,47 @@
+using CSharpFunctionalExtensions;
+using DirectoryService.Application.Departments.Repositories;
+using DirectoryService.Application.Extensions.Validation;
+using DirectoryService.Shared.Errors;
 using FluentValidation;
 
 namespace DirectoryService.Application.Departments.MoveDepartments;
 
 public class UpdateDepartmentPathValidator : AbstractValidator<UpdateDepartmentPathCommand>
 {
-    public UpdateDepartmentPathValidator()
+    private readonly IDepartmentRepository _departmentRepository;
+    public UpdateDepartmentPathValidator(IDepartmentRepository departmentRepository)
     {
+        _departmentRepository = departmentRepository;
+
+        RuleFor(x => x.DepartmentId)
+            .NotNull()
+            .NotEmpty()
+            .WithError(Error.Validation("department.id.is.null", "DepartmentId must not be empty."));
         
+        
+        RuleFor(x => x.DepartmentId)
+            .MustAsync(async (departmentId, cancellecion) =>
+            {
+                var isExist = await _departmentRepository.ExistDepartment(departmentId, cancellecion);
+
+                return isExist is { IsSuccess: true };
+            })
+            .WithError(Error.NotFound("department.not.found", "Department not found."));
+        
+        When(x => x.ParentId.HasValue, () =>
+        {
+            RuleFor(x => x.ParentId!.Value)
+                .MustAsync(async (parentId, cancellecion) =>
+                {
+                   UnitResult<Error> result = await _departmentRepository.ExistDepartment(parentId, cancellecion);
+
+                   return result.IsSuccess;
+                })
+                .WithError(Error.NotFound("department.not.found", "Department not found."));
+        });
+
+        RuleFor(x => x)
+            .Must(x => x.ParentId is null || x.ParentId != x.DepartmentId)
+            .WithError(Error.Validation("parent.id.is.the.same.as.department.id", "ParentId must not be the same as DepartmentId."));
     }
 }
